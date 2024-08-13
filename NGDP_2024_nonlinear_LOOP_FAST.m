1eba3d3a9a953d7684ae06556b18d7bd5221c4e2
Michael Hatcher <m.c.hatcher@soton.ac.uk>
2024-08-13 11:13:36 +0100

    Add files via upload

--- NGDP_2024_nonlinear_LOOP_FAST.m
+++ NGDP_2024_nonlinear_LOOP_FAST.m
@@ -1,6 +1,6 @@
-%NGDP_2024_nonlinear_LOOP
-%Nonlinear simulation of the simple model (Algorithm in Supp Appendix).
-%Compute means of variables and welfare loss of NGDP over many simulations. 
+%NGDP_2024_nonlinear_LOOP_FAST
+%Nonlinear simulation of the simple model (Algorithm in Supp Appendix),
+%this faster version of the code avoids stacking over many simulations. 
 %Written by Michael Hatcher (m.c.hatcher@soton.ac.uk). Any errors are my own.
 
 %clear
@@ -21,6 +21,7 @@ sig_A = 0.05;
 
 n_sim = 100;
 T_sim = 310; drop = 10;
+T_sim2 = T_sim - drop;
 
 N_guess0 = 610;
 N_guess = 800;
@@ -46,11 +47,11 @@ x1 = e_i;
 y_prime = ybar*exp(states(:,2));
 
 Stack_utility = NaN(n_loop,1); Stack_tau = Stack_utility; Stack_ftau = Stack_utility; Stack_b = Stack_utility; 
-Stack_c1 = Stack_utility; Stack_c2 = Stack_utility; var_utility = Stack_b; var_tau = Stack_b; var_ftau = Stack_b; 
-Lambda = Stack_utility; U_IT = Stack_b; tau_IT = U_IT; ftau_IT = U_IT; vtau_IT = U_IT; c1_IT = U_IT; c2_IT = U_IT;
-vc1_IT = U_IT; vc2_IT = U_IT; var_c1 = Stack_utility; var_c2 = Stack_utility; U_mean_NIT = var_c1; ftau_mean = var_c1;
+Stack_c1 = Stack_utility; Stack_c2 = Stack_utility; var_tau = Stack_b; var_ftau = Stack_b; Lambda = Stack_utility;
+U_IT = Stack_b; tau_IT = U_IT; ftau_IT = U_IT; vtau_IT = U_IT; c1_IT = U_IT; c2_IT = U_IT; vc1_IT = U_IT; vc2_IT = U_IT; 
+var_c1 = Stack_utility; var_c2 = Stack_utility; U_mean_NIT = var_c1; ftau_mean = var_c1;
 
-Resid_max = NaN(n_loop,1); Lambda_NIT = Resid_max; Index_mini = NaN(n_loop,1); Index_maxi = Resid_max; Resid_max2 = Resid_max;  
+Resid_max = U_IT; Lambda_NIT = Resid_max; Index_mini = Resid_max; Index_maxi = Resid_max; Resid_max2 = Resid_max;  
 Lambda_tau = Resid_max; Lambda_mean_NIT = Resid_max; Lambda_var_NIT = Resid_max; U_mean_IT = Resid_max;
 
 for m=1:n_loop
@@ -71,8 +72,9 @@ for m=1:n_loop
     %---------------------------
     %Stochastic simulations
     %---------------------------
-    Dum = zeros(n_sim,1); Max_resid = NaN(n_sim,1); Index_loc = Max_resid; Index_loc2 = Max_resid;  
-    c1_vec = []; c2_vec = []; Utility_vec = []; tau_vec = []; ftau_vec = [];
+    Dum = zeros(n_sim,1); Max_resid = Dum; Index_loc = Dum; Index_loc2 = Dum;  
+    c1_vec = Dum; c2_vec =  Dum; tau_vec =  Dum; ftau_vec =  Dum; Utility_vec =  Dum;
+    c1_vec2 = Dum; c2_vec2 =  Dum; tau_vec2 =  Dum; ftau_vec2 =  Dum; 
 
     for j=1:n_sim
 
@@ -87,11 +89,19 @@ for m=1:n_loop
         Index_loc2(j) = min(Index);
     
         %Store and stack key variables
-        c1_vec = [c1(drop+1:end); c1_vec];
-        c2_vec = [c2(drop+1:end); c2_vec];
-        tau_vec = [tau(drop+1:end); tau_vec];
-        ftau_vec = [ftau(drop+1:end); ftau_vec];
-        Utility_vec = [Utility(drop+1:end); Utility_vec];
+
+        %For means
+        c1_vec(j) = sum(c1(drop+1:end));
+        c2_vec(j) = sum(c2(drop+1:end));
+        tau_vec(j) = sum(tau(drop+1:end));
+        ftau_vec(j) = sum(ftau(drop+1:end));
+        Utility_vec(j) = sum(Utility(drop+1:end));
+
+        %For variances
+        c1_vec2(j) = sum(c1(drop+1:end).^2);
+        c2_vec2(j) = sum(c2(drop+1:end).^2);
+        tau_vec2(j) = sum(tau(drop+1:end).^2);
+        ftau_vec2(j) = sum(ftau(drop+1:end).^2);
 
     end
 
@@ -101,15 +111,16 @@ for m=1:n_loop
     Index_mini(m) = min(Index_loc2);
     Index_maxi(m) = max(Index_loc);
 
-    Stack_utility(m) = mean(Utility_vec);
-    Stack_c1(m) = mean(c1_vec);
-    Stack_c2(m) = mean(c2_vec);
-    Stack_tau(m) = mean(tau_vec);
-    Stack_ftau(m) = mean(ftau_vec);
-    var_c1(m) = var(c1_vec);
-    var_c2(m) = var(c2_vec);
-    var_tau(m) = var(tau_vec);
-    var_ftau(m) = var(ftau_vec);
+    Stack_utility(m) = sum(Utility_vec)/(n_sim*T_sim2);
+    Stack_c1(m) = sum(c1_vec)/(n_sim*T_sim2);
+    Stack_c2(m) = sum(c2_vec)/(n_sim*T_sim2);
+    Stack_tau(m) = sum(tau_vec)/(n_sim*T_sim2);
+    Stack_ftau(m) = sum(ftau_vec)/(n_sim*T_sim2);
+    
+    var_c1(m) = sum(c1_vec2)/(n_sim*T_sim2) - (Stack_c1(m))^2;
+    var_c2(m) = sum(c2_vec2)/(n_sim*T_sim2) - (Stack_c2(m))^2;
+    var_tau(m) = sum(tau_vec2)/(n_sim*T_sim2) - (Stack_tau(m))^2;
+    var_ftau(m) = sum(ftau_vec2)/(n_sim*T_sim2) - (Stack_ftau(m))^2;
 
 end
 
